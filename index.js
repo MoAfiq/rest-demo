bfdacff9597d5582861130c1615909eabf270f23
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-07-14 14:26:05 +0800

    Improve the UX

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 app.use("/kv", require("./kv.js"));
 app.use("/todos", require("./todos.js"));
+app.use((req, res) => {
+  res.status(200).end("This is the demo server. See README.md for more info");
+});
 app.listen(8080);
+console.log("Server mounted at localhost:8080");
